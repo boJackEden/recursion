fb2f693f4c3dd4e027424882508ef3b533f68f9c
Eden Mazzola <mazzolaeden@gmail.com>
2015-03-13 01:15:45 -0700

    "Finished getElementByClass. True recursion!"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, domNode, result){
+
+	domNode = domNode || document
+	result = result || [];
+	
+    var children = domNode.children;
+    for(var i = 0; i < children.length; i++) {
+      if(children[i].classList.contains(className)) {
+        result.push(children[i]);
+      }
+      if(children[i].hasChildNodes) {
+        getElementsByClassName(className, children[i], result);
+      }
+    }
+  return result;
 };
+
+
